1a50629ade3b166d5bf031090f4aa670a42f4344
Tambien42 <tambien_36@hotmail.com>
2023-03-29 23:51:20 +0200

    add restart when error occurs

--- Assemblee_Nationale/law_proposals.py
+++ Assemblee_Nationale/law_proposals.py
@@ -63,56 +63,61 @@ def save_to_database(data: dict, Model):
     session.close()
 
 def law_proposals():
-    url = 'https://www2.assemblee-nationale.fr/documents/liste/(type)/propositions-loi'
+    try:
+        url = 'https://www2.assemblee-nationale.fr/documents/liste/(type)/propositions-loi'
 
-    data = {}
-    while True:
-        soup = make_request(url)
-        list = soup.find('ul', class_='liens-liste').find_all('li', recursive=False)
-        for law in list:
-            # Extract the number of the proposal
-            data['number'] = law.find('h3').text.split('-')[-1].replace(u'\xa0', ' ').split(' ')[-1]
-            # Extract title
-            title = law.find('h3').text
-            data['title'] = title.split('-')[0]
-            if len(title.split('-')) > 2:
-                title_array = title.split('-')[:-1]
-                data['title'] = '-'.join(title_array)
-            # Extract link
-            data['link'] = law.find('ul').find_all('li')[-1].find('a')['href']
-            # Extract date
-            string = law.find('ul').find_all('li')[0].text.strip()
-            # Define regular expression pattern to match the date
-            pattern = r"(\d{1,2}\s\w+\s\d{4})"
-            # Find the date in the string
-            match = re.search(pattern, string)
-            if match:
-                date = match.group(1)
-                data['date'] = datetime.strptime(date, "%d %B %Y")
-            else:
-                # handle the case where the regex didn't match
-                data['date'] = None
-            
-            # Extract author and cosigner of law proposals
-            url_dossier = law.find_all("li")[-2].find("a")["href"]
-            signer_page = make_request(url_dossier)
-            signer = signer_page.find('div', class_='carrousel-auteurs-rapporteurs')
-            data['author'] = ''
-            data['cosigner'] = ''
-            if signer != None:
-                data['author'] = signer.find_all('li')[0].text.strip()
-                for author in signer.find_all('li')[1:]:
-                    data['author'] = data['author'] + ', ' + author.find("p", class_="nom-personne").text.strip()
+        data = {}
+        while True:
+            soup = make_request(url)
+            list = soup.find('ul', class_='liens-liste').find_all('li', recursive=False)
+            for law in list:
+                # Extract the number of the proposal
+                data['number'] = law.find('h3').text.split('-')[-1].replace(u'\xa0', ' ').split(' ')[-1]
+                # Extract title
+                title = law.find('h3').text
+                data['title'] = title.split('-')[0]
+                if len(title.split('-')) > 2:
+                    title_array = title.split('-')[:-1]
+                    data['title'] = '-'.join(title_array)
+                # Extract link
+                data['link'] = law.find('ul').find_all('li')[-1].find('a')['href']
+                # Extract date
+                string = law.find('ul').find_all('li')[0].text.strip()
+                # Define regular expression pattern to match the date
+                pattern = r"(\d{1,2}\s\w+\s\d{4})"
+                # Find the date in the string
+                match = re.search(pattern, string)
+                if match:
+                    date = match.group(1)
+                    data['date'] = datetime.strptime(date, "%d %B %Y")
+                else:
+                    # handle the case where the regex didn't match
+                    data['date'] = None
                 
-                if signer.find('a', {'data-toggle':'modal'}):
-                    modal = signer_page.find('div', id='cosignataires').find('div', id="cosignataires-liste").find_all('a')
-                    data['cosigner'] = modal[0].text.strip()
-                    for cosign in modal[1:]:
-                        data['cosigner'] = data['cosigner'] + ', ' + cosign.text.strip()
-            
-            save_to_database(data, Law_proposals)
+                # Extract author and cosigner of law proposals
+                url_dossier = law.find_all("li")[-2].find("a")["href"]
+                signer_page = make_request(url_dossier)
+                signer = signer_page.find('div', class_='carrousel-auteurs-rapporteurs')
+                data['author'] = ''
+                data['cosigner'] = ''
+                if signer != None:
+                    data['author'] = signer.find_all('li')[0].text.strip()
+                    for author in signer.find_all('li')[1:]:
+                        data['author'] = data['author'] + ', ' + author.find("p", class_="nom-personne").text.strip()
+                    
+                    if signer.find('a', {'data-toggle':'modal'}):
+                        modal = signer_page.find('div', id='cosignataires').find('div', id="cosignataires-liste").find_all('a')
+                        data['cosigner'] = modal[0].text.strip()
+                        for cosign in modal[1:]:
+                            data['cosigner'] = data['cosigner'] + ', ' + cosign.text.strip()
+                
+                save_to_database(data, Law_proposals)
 
-        # Get next page
-        url = next_page(soup)
-        if url is None:
-            break       
+            # Get next page
+            url = next_page(soup)
+            if url is None:
+                break
+
+    except Exception as e:
+        print(f'An error occurred, restarting law proposals scraping...')
+        law_proposals()
